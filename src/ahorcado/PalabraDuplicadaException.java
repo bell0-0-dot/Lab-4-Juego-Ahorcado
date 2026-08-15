a6a1cf4a2a8b1419401bff12907a591e634ecbf9
GGutierrez06 <gabriel_gutierrez@unitec.edu>
2026-08-15 11:39:16 -0600

    Implementacion de juego azar

---
+++ src/ahorcado/PalabraDuplicadaException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ahorcado;
+
+/**
+ *
+ * @author gabri
+ */
+public class PalabraDuplicadaException extends Exception{
+    public PalabraDuplicadaException(String mensaje) {
+        super(mensaje);
+    }
+}
